f4ffbf4b3375203e4af0e3aeda49a7a00c6cfdb6
Steph Wang <sw.steph@gmail.com>
2016-12-24 16:21:56 +0000

    first pass at habit card

--- project/app.py
+++ project/app.py
@@ -8,10 +8,5 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
