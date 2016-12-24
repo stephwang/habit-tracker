222704a827f08f9fedfc6f5b6222c6687fed80df
Steph Wang <sw.steph@gmail.com>
2016-12-24 20:58:36 +0000

    added DB boilerplate

--- project/app.py
+++ project/app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -17,5 +19,5 @@ def signup():
     return render_template('signup.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000)) # set to 8080 when working in cloud 9  
+    port = int(os.environ.get("PORT", 8080)) # set to 5000 for heroku, 8080 when working in cloud 9  
     app.run(host='0.0.0.0', port=port, debug=True)
