8090502af22c209e0f370559d5e4d171dacdb9e3
Steph Wang <sw.steph@gmail.com>
2016-12-25 01:47:31 +0000

    trying to get this to work on heroku

--- project/app.py
+++ project/app.py
@@ -6,7 +6,8 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-MONGO_URL = os.environ.get('MONGO_URL')
+MONGO_URL = os.environ.get('MONGODB_URI')
+
 app.config['MONGO_URI'] = MONGO_URL
 mongo = PyMongo(app)
 
